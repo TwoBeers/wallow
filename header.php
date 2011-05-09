89a914b69cd87519a0ae948c1a87db3c0c883f43
ilmaren <ilmaren@clansabbat.it>
2011-05-09 11:42:47 +0200

    v 0.46.1
    fix come da review http://themes.trac.wordpress.org/ticket/3749#comment:2

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
+<html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes( 'xhtml' ); ?>>
 
 <head profile="http://gmpg.org/xfn/11">
 	<meta http-equiv="Content-Type" content="<?php bloginfo( 'html_type' ); ?>; charset=<?php bloginfo( 'charset' ); ?>" />
@@ -19,7 +19,7 @@
 	
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 	<?php wp_get_archives( 'type=monthly&format=link' ); ?>
-	<?php // if theme option is active, loads js animation
+	<?php // call for theme options
 		$wallow_opt = get_option( 'wallow_options' );
 	?> 
 	<?php wp_head(); ?>
